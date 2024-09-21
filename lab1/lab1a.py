cc01c421b9b10fe18bf9f9fe821263d59ab26173
Krishan Sivanathan <ksivanathan3@myseneca.ca>
2024-09-21 13:58:34 -0400

    lab1a python code

---
+++ lab1/lab1a.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''Description: This program will output "hello world" to the screen.
+Krishan Sivanathan
+OPS445'''
+
+# Krishan Sivanathan
+# OPS445
+
+print('hello world')
